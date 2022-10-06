8d14eb42cc77bdf1dd188f42581fa68e4cd1f518
Andrew Busel <buselax@gmail.com>
2022-10-05 20:31:10 -0400

    response for form

--- src/Pages/ContactPage.tsx
+++ src/Pages/ContactPage.tsx
@@ -1,4 +1,5 @@
 import { ThemeContext } from "@emotion/react";
+import { Alert, CircularProgress, Snackbar } from "@mui/material";
 import { useState } from "react";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
@@ -7,6 +8,10 @@ export default function ContactPage() {
   let [name, setName] = useState("");
   let [email, setEmail] = useState("");
   let [message, setMessage] = useState("");
+  let [loading, setLoading] = useState(false);
+  let [response, setResponse] = useState<"success" | "error" | undefined>(
+    undefined
+  );
   return (
     <div
       id="wrapper"
@@ -19,95 +24,120 @@ export default function ContactPage() {
         <div className="logo">Get Involved</div>
       </header>
       <footer id="footer" style={{ marginBottom: 60 }}>
-        <section>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              fetch(
-                "https://sheet.best/api/sheets/88790b60-f222-4a49-ac55-4c0205217574",
-                {
-                  method: "POST",
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                  body: JSON.stringify({
-                    Name: name,
-                    Email: email,
-                    Message: message,
-                  }),
-                }
-              ).then((res) => {
-                console.log(res);
-              });
-            }}
-          >
-            <div className="fields">
-              <div
-                className="field"
-                style={{ display: "flex", flexDirection: "column" }}
-              >
-                <label style={{ marginRight: "auto", marginLeft: "30px" }}>
-                  Name
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  value={name}
-                  onChange={(e) => {
-                    setName(e.target.value);
-                  }}
-                />
-              </div>
-              <div className="field">
-                <label style={{ marginRight: "auto", marginLeft: "30px" }}>
-                  Email
-                </label>
-                <input
-                  type="text"
-                  name="email"
-                  id="email"
-                  value={email}
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                />
-              </div>
-              <div className="field">
-                <label style={{ marginRight: "auto", marginLeft: "30px" }}>
-                  Message
-                </label>
-                <textarea
-                  name="message"
-                  id="message"
-                  rows={3}
-                  value={message}
-                  onChange={(e) => {
-                    setMessage(e.target.value);
-                  }}
-                ></textarea>
-              </div>
+        <section style={{ display: "flex", flexDirection: "column" }}>
+          {loading ? (
+            <div style={{ width: "100%", display: "flex", height: 491 }}>
+              <CircularProgress
+                sx={{
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  marginTop: "auto",
+                  marginBottom: "auto",
+                }}
+              />
             </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" />
-              </li>
-            </ul>
-          </form>
+          ) : (
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault();
+                setLoading(true);
+                let response = await fetch(
+                  "https://sheet.best/api/sheets/88790b60-f222-4a49-ac55-4c0205217574",
+                  {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                      Name: name,
+                      Email: email,
+                      Message: message,
+                    }),
+                  }
+                );
+                setLoading(false);
+                if (response.ok) {
+                  setResponse("success");
+                  setMessage("");
+                  setEmail("");
+                  setName("");
+                } else {
+                  setResponse("error");
+                }
+              }}
+            >
+              <div className="fields">
+                <div
+                  className="field"
+                  style={{ display: "flex", flexDirection: "column" }}
+                >
+                  <label style={{ marginRight: "auto", marginLeft: "30px" }}>
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={name}
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="field">
+                  <label style={{ marginRight: "auto", marginLeft: "30px" }}>
+                    Email
+                  </label>
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="field">
+                  <label style={{ marginRight: "auto", marginLeft: "30px" }}>
+                    Message
+                  </label>
+                  <textarea
+                    name="message"
+                    id="message"
+                    rows={3}
+                    value={message}
+                    onChange={(e) => {
+                      setMessage(e.target.value);
+                    }}
+                  ></textarea>
+                </div>
+              </div>
+              <ul className="actions">
+                <li>
+                  <input type="submit" value="Send Message" />
+                </li>
+              </ul>
+            </form>
+          )}
         </section>
       </footer>
+      {response && (
+        <Snackbar
+          open={!!response}
+          autoHideDuration={2000}
+          onClose={() => {
+            setResponse(undefined);
+          }}
+        >
+          <Alert severity={response}>
+            {response === "success"
+              ? "Thank you for submission"
+              : "Something went wrong"}
+          </Alert>
+        </Snackbar>
+      )}
       <Footer />
     </div>
   );
-  {
-    /* 
-  <div id="copyright">
-    <ul>
-      <li>&copy; Untitled</li>
-      <li>
-        Design: <a href="https://html5up.net">HTML5 UP</a>
-      </li>
-    </ul>
-  </div> */
-  }
 }
